b1ea5be024e9d45bc3fae6f53fb28c3ab05913fa
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-04-15 11:37:22 +0200

    added actionmethod to support adding a new event rating

---
+++ src/Nitrilon/Nitrilon.Api/Controllers/EventRatingsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Nitrilon.DataAccess;
+using Nitrilon.Entities;
+
+namespace Nitrilon.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventRatingsController: Controller
+    {
+        [HttpPost]
+        public IActionResult AddEventRating(int eventId, int ratingId)
+        {
+            try
+            {
+                Repository r = new();
+                int createdId = r.SaveEventRating(eventId, ratingId);
+                return Ok(createdId);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
