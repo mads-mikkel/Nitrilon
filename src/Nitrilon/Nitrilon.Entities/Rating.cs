d9fea51e375683c9df241e3d92d66bde72360090
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-04-18 13:21:33 +0200

    created the Rating class

---
+++ src/Nitrilon/Nitrilon.Entities/Rating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitrilon.Entities
+{
+    public class Rating
+    {
+        private int id;
+        private int ratingValue;
+        private string description;
+
+        public Rating(int id, int ratingValue, string description)
+        {
+            Id = id;
+            RatingValue = ratingValue;
+            Description = description;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int RatingValue { get => ratingValue; set => ratingValue = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
