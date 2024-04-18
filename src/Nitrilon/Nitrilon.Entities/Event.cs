92a16dee72b98336310028cef3ee23fedf92834b
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-04-18 11:22:34 +0200

    added encapsulation to Event class

--- src/Nitrilon/Nitrilon.Entities/Event.cs
+++ src/Nitrilon/Nitrilon.Entities/Event.cs
@@ -2,10 +2,90 @@
 {
     public class Event
     {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int Attendees { get; set; }
+        #region Fields and constants
+        public readonly DateTime EarliestPossibleEvent = new DateTime(2018, 1, 1);
+        private int id;
+        private string name;
+        private DateTime date;
+        private int attendees;
+        private string description;
+        #endregion
+
+        #region Constructors
+        public Event(int id, string name, DateTime date, int attendees, string description)
+        {
+            Id = id;
+            Name = name;
+            Date = date;
+            Attendees = attendees;
+            Description = description;
+        }
+        #endregion
+
+        #region Properties
+        public int Id
+        {
+            get => id;
+            private set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if(id != value)
+                {
+                    id = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if(name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, EarliestPossibleEvent);
+                if(date != value)
+                {
+                    date = value;
+                }
+            }
+        }
+
+        public int Attendees
+        {
+            get => attendees;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, -1);
+                if(attendees != value)
+                {
+                    attendees = value;
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if(description != value)
+                {
+                    description = value;
+                }
+            }
+        } 
+        #endregion
     }
 }
