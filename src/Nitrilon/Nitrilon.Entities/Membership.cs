628766955574837a7114b3de705df8eb36ac2c77
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-05-06 14:35:00 +0200

    added member and membership classes

---
+++ src/Nitrilon/Nitrilon.Entities/Membership.cs
@@ -0,0 +1,20 @@
+﻿namespace Nitrilon.Entities
+{
+    public class Membership 
+    {
+        private int id;
+        private string name;
+        private string description;
+
+        public Membership(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
